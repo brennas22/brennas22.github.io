045fa79759f3ba2a77c4f3ce03d476c2e94e1c2f
brennas22 <brenna.sorkin@gmail.com>
2017-02-01 21:45:58 -0500

    huge update

---
+++ assets/js/custom.js
@@ -0,0 +1,89 @@
+(function($) {
+
+// prettyPhoto
+	jQuery(document).ready(function(){
+		jQuery('a[data-gal]').each(function() {
+			jQuery(this).attr('rel', jQuery(this).data('gal'));
+		});  	
+		jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({animationSpeed:'slow',theme:'light_square',slideshow:false,overlay_gallery: false,social_tools:false,deeplinking:false});
+	}); 
+
+		
+})(jQuery);
+
+//menu fade on scroll!
+(function ($) {
+  $(document).ready(function(){
+
+    // hide .navbar first
+    $(".navbar").hide();
+
+    // fade in .navbar
+    $(function () {
+        $(window).scroll(function () {
+
+                 // set distance user needs to scroll before we start fadeIn
+            if ($(this).scrollTop() > 100) {
+                $('.navbar').fadeIn();
+            } else {
+                $('.navbar').fadeOut();
+            }
+        });
+    });
+
+});
+  }(jQuery));
+
+	// Text Rotator
+	$('.rotate').each(function(){
+		var el = $(this);
+		var text = $(this).html().split(",");
+		el.html(text[0]);
+		setInterval(function() {
+			el.animate({
+              textShadowBlur:20,
+              opacity: 0
+            }, 500 , function() {
+              index = $.inArray(el.html(), text)
+              if((index + 1) == text.length) index = -1
+              el.text(text[index + 1]).animate({
+                textShadowBlur:0,
+                opacity: 1
+              }, 500 );
+            });
+		}, 1800);
+	});
+
+
+		// Scroll Down Link
+	$('.scrolldown').click(function() {
+		var targetOffset = $('#service').offset().top-80;
+		$('html,body').animate({scrollTop: targetOffset}, 1000);
+	});
+
+			// Scroll Down Link2
+	$('.scrolldown2').click(function() {
+		var targetOffset = $('#div_element2').offset().top-80;
+		$('html,body').animate({scrollTop: targetOffset}, 1000);
+	});
+
+	// Instantiate the Bootstrap carousel
+$('.multi-item-carousel').carousel({
+  interval: false
+});
+
+// for every slide in carousel, copy the next slide's item in the slide.
+// Do the same for the next, next item.
+$('.multi-item-carousel .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  if (next.next().length>0) {
+    next.next().children(':first-child').clone().appendTo($(this));
+  } else {
+  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+  }
+});
