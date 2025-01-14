0af5a9727ce13d5d1cf57ad9555d42a4436be618
brennas22 <brenna.sorkin@gmail.com>
2025-01-14 10:47:42 -0500

    update homepage

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+// import Navbar from './Navbar';
+// import Portfolio from './Portfolio';
+// import ManagerCloseOut from './ManagerCloseOut'; // Import individual project pages
+// import GiftingProgram from './GiftingProgram'; // Import individual project pages
+// import AdminTool from './AdminTool'; // Import individual project pages
+// import AboutMe from './AboutMe'; // Import individual project pages
+import HomePage from './HomePage'; // Import individual project pages
+
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route index element={<HomePage />} /> {/* HomePage will be the default route */}
+        {/* <Route path="/manager-close-out" element={<ManagerCloseOut />} />
+        <Route path="/gifting-program" element={<GiftingProgram />} />
+        <Route path="/admin-tool" element={<AdminTool />} />
+        <Route path="/about-me" element={<AboutMe />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
